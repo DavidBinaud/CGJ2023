cda18c968d33a0462c478cfb18752f8650088ee5
David Binaud <david.binaud@etu.umontpellier.fr>
2023-01-24 22:35:35 +0100

    Add TimeFreeze and Tornado
    Need to use freeze on enemies
    
    tornado - need to apply damages

--- Assets/Scripts/Spells/TimeFreezeSpell.cs
+++ Assets/Scripts/Spells/TimeFreezeSpell.cs
@@ -4,8 +4,26 @@ using UnityEngine;
 
 public class TimeFreezeSpell : MonoBehaviour, ISpell
 {
+    
     public void Cast()
     {
+        Vector3 playerPosition = PlayerController.Instance.transform.position;
+        List<GameObject> enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+        List<GameObject> enemiesInRange =
+            enemies.FindAll((e) => Vector3.Distance(e.transform.position, playerPosition) < 50);
+        
+        /*
+         Freeze the enemies
+         * foreach (var enemy in enemiesInRange)
+        {
+            enemy.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
+        }
+        */
         Debug.Log("TimeFreeze !");
+    }
+
+    public void unFreeze()
+    {
+        
     }
 }
